70becbacdc53471dd5408d48a3219c017bbf0889
BarbedMach <furkanozdemir.xyz@gmail.com>
2024-10-07 17:16:38 +0300

    simplified the project structure

--- main.go
+++ main.go
@@ -22,6 +22,14 @@ type Config struct {
 	DatabaseName string `json:"DATABASE_NAME"`
 }
 
+type WorkItem struct {
+	ID          int    `json:"id"`
+	WorkDate    string `json:"workDate"`
+	StartTime   string `json:"startTime"`
+	EndTime     string `json:"endTime"`
+	Description string `json:"description"`
+}
+
 func main() {
 	var err error
 
--- models.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type WorkItem struct {
-	ID          int    `json:"id"`
-	WorkDate    string `json:"workDate"`
-	StartTime   string `json:"startTime"`
-	EndTime     string `json:"endTime"`
-	Description string `json:"description"`
-}
